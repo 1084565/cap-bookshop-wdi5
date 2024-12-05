70cd01fbae328e8b356ada345eac97cef6ec5985
Christoph Szymanski <christoph.szymanski@sap.com>
2024-12-05 15:21:34 +0100

    Update blackduck.sh

--- scripts/blackduck.sh
+++ scripts/blackduck.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
-
 set -e
-
-# Function to install JRE
-install_jre() {
-
 
 JRE_VERSION="21.0.5" # You can change this to the desired version
 JRE_TAR="sapmachine-jre-${JRE_VERSION}_linux-x64_bin.tar.gz"
+
+
+# Function to install JRE
+install_jre() {
 # resolves to https://github.com/SAP/SapMachine/releases/download/sapmachine-21.0.5/sapmachine-jre-21.0.5_linux-x64_bin.tar.gz
 JRE_URL="https://github.com/SAP/SapMachine/releases/download/sapmachine-$JRE_VERSION/$JRE_TAR"
-echo "Downloading and installing sapmachine at ... " $JRE_URL
+echo "Downloading and installing sapmachine from " $JRE_URL
 
-wget $JRE_URL
+wget --quiet $JRE_URL
 tar -xzf $JRE_TAR
 rm $JRE_TAR
 
-ls -lah
+#ls -lah
 
-export JAVA_HOME=$(pwd)/sapmachine-jre-21.0.5
-export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=$(pwd)/sapmachine-jre-${JRE_VERSION}
+#export PATH=$JAVA_HOME/bin:$PATH
 
-ls -lah $JAVA_HOME
+#ls -lah $JAVA_HOME
 
-echo "Java installation completed."
+echo "Java installation complete."
 $JAVA_HOME/bin/java -version
 }
 
 # Function to download the Black Duck Detect script
 download_detect_tool() {
-echo "Downloading Black Duck Detect tool..."
 DETECT_URL="https://detect.synopsys.com/detect9.sh" # Official Detect download URL
-wget $DETECT_URL -O detect.sh
+
+echo "Downloading Black Duck Detect tool from " $DETECT_URL
+
+wget --quiet $DETECT_URL -O detect.sh
 chmod +x detect.sh
-echo "Black Duck Detect tool downloaded."
+echo "Black Duck Detect tool download complete."
 }
 
 # Function to run Black Duck scan
