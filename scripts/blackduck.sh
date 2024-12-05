92bede5bd7f7e3996042ed556018147b2d39c563
Christoph Szymanski <christoph.szymanski@sap.com>
2024-12-05 16:15:30 +0100

    Update blackduck.sh

--- scripts/blackduck.sh
+++ scripts/blackduck.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
 set -e
 
-JRE_VERSION="21.0.5" # You can change this to the desired version
+JRE_VERSION="21.0.5" 
 JRE_TAR="sapmachine-jre-${JRE_VERSION}_linux-x64_bin.tar.gz"
-
-
-# Function to install JRE
-install_jre() {
-# resolves to https://github.com/SAP/SapMachine/releases/download/sapmachine-21.0.5/sapmachine-jre-21.0.5_linux-x64_bin.tar.gz
 JRE_URL="https://github.com/SAP/SapMachine/releases/download/sapmachine-$JRE_VERSION/$JRE_TAR"
-echo "Downloading and installing sapmachine from " $JRE_URL
 
-wget --quiet $JRE_URL
-tar -xzf $JRE_TAR
-rm $JRE_TAR
+DETECT_URL="https://detect.synopsys.com/detect10.sh" # Official Detect download URL
 
-#ls -lah
+install_jre() {
+  echo "Downloading and installing sapmachine from " $JRE_URL
+  
+  wget --quiet $JRE_URL
+  tar -xzf $JRE_TAR
 
-export JAVA_HOME=$(pwd)/sapmachine-jre-${JRE_VERSION}
-#export PATH=$JAVA_HOME/bin:$PATH
+  export JAVA_HOME=$(pwd)/sapmachine-jre-${JRE_VERSION}
+  $JAVA_HOME/bin/java -version
 
-#ls -lah $JAVA_HOME
-
-echo "Java installation complete."
-$JAVA_HOME/bin/java -version
+  echo "Java installation complete."
 }
 
-# Function to download the Black Duck Detect script
 download_detect_tool() {
-DETECT_URL="https://detect.synopsys.com/detect9.sh" # Official Detect download URL
+  echo "Downloading Black Duck Detect tool from " $DETECT_URL
 
-echo "Downloading Black Duck Detect tool from " $DETECT_URL
-
-wget --quiet $DETECT_URL -O detect.sh
-chmod +x detect.sh
-echo "Black Duck Detect tool download complete."
+  wget --quiet $DETECT_URL -O detect.sh
+  chmod +x detect.sh
+  
+  echo "Black Duck Detect tool download complete."
 }
 
-# Function to run Black Duck scan
 run_blackduck_scan() {
-echo "Running Black Duck scan..."
+  echo "Running Black Duck scan..."
 
-./detect.sh --blackduck.url="$BD_SERVER" --blackduck.api.token="$BD_API_TOKEN" --detect.project.name="your-project-name" --detect.project.version.name="your-version-name"
-echo "Black Duck scan completed."
+  ./detect.sh --blackduck.url="$BD_SERVER" --blackduck.api.token="$BD_API_TOKEN" --detect.project.name="your-project-name" --detect.project.version.name="your-version-name"
+  echo "Black Duck scan completed."
 }
 
 # Main script execution
